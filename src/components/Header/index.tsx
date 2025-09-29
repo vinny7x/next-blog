aae3d7a6f7590bddd88eb495b8f8457bd6b63891
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-09-29 16:55:05 -0300

    🔧 chore: pacote clsx

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+import clsx from "clsx";
+
+export function Header(){
+    return (
+        <h1 className={clsx(
+                'text-blue-300',
+                'transform',
+                '',
+                'text-4xl',
+                'hover:bg-red-400',
+            )}>oi</h1>
+    )
+}
