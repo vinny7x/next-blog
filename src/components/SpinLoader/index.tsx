9e90bbfc2c982fbbea61d0829e221fd143795d1d
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-02 15:04:28 -0300

    :lipstick: style: componente loader

---
+++ src/components/SpinLoader/index.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+type SpinLoaderProps = {
+    className?: string
+}
+
+export function SpinLoader({className = ""}:SpinLoaderProps) {
+    return (
+        <div className={clsx(
+            'flex',
+            'items-center',
+            'justify-center',
+            className
+        )}>
+            <div className={clsx(
+                'w-10 h-10',
+                'border-5 border-t-transparent border-slate-900',
+                'rounded-full',
+                'animate-spin'
+            )}></div>
+        </div>
+    )
+}
