24da0c9453170fb8418ac159223830545707fe49
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-02 15:36:09 -0300

    ✨ feat: função para simular atraso na resposta[

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,28 +1,47 @@
-import { PostModel } from "@/models/post/post-model";
-import { PostRepository } from "./post-repository";
-import { resolve } from "path";
-import { readFile } from 'fs/promises'
+import { PostModel } from '@/models/post/post-model';
+import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
 
 const ROOT_DIR = process.cwd();
-const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json')
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+);
+const SIMULATE_WAIT_IN_MS = 5000;
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
 
-    private async readFromDisk(): Promise<PostModel[]> {
-        const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8')
-        const parsedJson = JSON.parse(jsonContent)
-        const { posts } = parsedJson
-        return posts
-    }
+    await new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
 
-    async findAll(): Promise<PostModel[]> {
-        const posts = await this.readFromDisk()
-        return posts
-    }
-    async findById(id: string): Promise<PostModel> {
-        const posts = await this.findAll()
-        const post = posts.find(post => post.id === id)
-        if (!post) throw new Error('Post não encontrado')
-        return post
-    }
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error('Post não encontrado');
+
+    return post;
+  }
 }
