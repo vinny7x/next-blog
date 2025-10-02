42b7903c16334aec9e45cb75711a555e40eba296
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-02 15:54:14 -0300

    ✨ feat: listagem de posts

---
+++ src/components/PostsList/index.tsx
@@ -0,0 +1,11 @@
+import { postRepository } from "@/repositories/post"
+
+export async function PostsList() {
+    const posts = await postRepository.findAll();
+    
+    return (
+        <div>
+            {posts.map(post => <p key={post.id}>{post.title}</p>)}
+        </div>
+    )
+}
