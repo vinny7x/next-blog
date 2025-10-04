6f27e11e13143a9d895e9577bf1bd6f93b16d01f
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-04 16:40:45 -0300

    ✨ feat: componente PostCoverImage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
-import { PostHeading } from "@/components/postHeading";
+import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
+
 
 export default async function HomePage() {
     return (
@@ -19,26 +19,7 @@ export default async function HomePage() {
                 'md:grid-cols-2',
                 'group'
             )}>
-                <Link
-                    className={clsx(
-                        'w-full h-full',
-                        'overflow-hidden',
-                        'rounded-xl'
-                    )}
-                    href="#">
-                    <Image
-                        className={clsx(
-                            'group-hover:scale-105 transition',
-                            'w-full h-full',
-                            'object-cover object-center'
-                        )}
-                        src='/images/bryen_0.png'
-                        width={1200}
-                        height={720}
-                        alt="Titulo do post"
-                        priority />
-                </Link>
-
+                <PostCoverImage src="/images/bryen_4.png" href='#' alt='Titulo do post' />
                 <div className={clsx(
                     'flex flex-col gap-4',
                     'sm:justify-center'
---
+++ src/components/PostCoverImage/index.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import Link from "next/link";
+import Image from "next/image";
+
+type PostCoverImageProps = {
+    href: string,
+    src: string,
+    alt: string
+}
+
+export function PostCoverImage({ href, src, alt }: PostCoverImageProps) {
+    return (
+        <Link
+            className={clsx(
+                'w-full h-full',
+                'overflow-hidden',
+                'rounded-xl'
+            )}
+            href={href}>
+            <Image
+                className={clsx(
+                    'group-hover:scale-105 transition',
+                    'w-full h-full',
+                    'object-cover object-center'
+                )}
+                src={src}
+                width={1200}
+                height={720}
+                alt={alt}
+                priority />
+        </Link>
+    )
+}
