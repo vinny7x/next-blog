1729b90dcad1660fc67b6caa9137cf2279bf6a70
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-13 17:59:12 -0300

    ✨ feat: página não encontrada

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+export default function NotFoundPage() {
+    return (
+        <div
+            className={clsx(
+                "bg-slate-900 text-slate-100",
+                "mb-16 p-8 rounded-xl",
+                "flex flex-col items-center justify-center",
+                "text-center gap-2",
+                "min-h-[320px]"
+            )}
+        >
+            <h1 className="text-7xl font-extrabold leading-tight">404</h1>
+            <p className="text-lg text-slate-300">
+                A página que você tentou acessar não foi encontrada.
+            </p>
+        </div>
+    );
+}
