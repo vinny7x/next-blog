7fb2f3e8e9f70ebb84c4614be560ab7f25d794da
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-13 17:31:34 -0300

    :recycle: refactor: realocação de elementos e metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,18 @@
-import { Container } from "@/components/Container";
 import { FeaturedPost } from "@/components/FeaturedPost";
-import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import clsx from "clsx";
 import { Suspense } from "react";
 
 
 export default async function HomePage() {
     return (
-        <Container>
-            <Header />
+        <>
             <Suspense fallback={<SpinLoader />}>
                 <FeaturedPost />
             </Suspense>
             <Suspense fallback={<SpinLoader />}>
                 <PostsList />
             </Suspense>
-            <footer>
-                <p className={clsx(
-                    'text-6xl font-bold text-center',
-                    'py-8'
-                )}>footer</p>
-            </footer></Container>
+        </>
     );
 }
