57b95459ae82e0676187c43f71230f448ce6e646
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 16:09:43 -0300

    :recycle: refactor: removendo importação desnecessária

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,7 +2,6 @@ import { SinglePost } from "@/components/SinglePost";
 import { SpinLoader } from "@/components/SpinLoader";
 import { findPostBySlugCached } from "@/lib/post/queries";
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 type PostSlugPageProps = {
