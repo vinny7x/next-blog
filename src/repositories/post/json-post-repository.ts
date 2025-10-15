140e49bb836ffbeb7213803f19e07f5db45f33e9
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 15:51:06 -0300

    :recycle: refactor: removendo duplicação de simulateWait

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,7 +11,7 @@ const JSON_POSTS_FILE_PATH = resolve(
     'seed',
     'posts.json',
 );
-const SIMULATE_WAIT_IN_MS = 0;
+const SIMULATE_WAIT_IN_MS = 5000;
 
 export class JsonPostRepository implements PostRepository {
 
@@ -35,8 +35,6 @@ export class JsonPostRepository implements PostRepository {
     }
 
     async findById(id: string): Promise<PostModel> {
-        await this.simulateWait();
-
         const posts = await this.findAllPublic();
         const post = posts.find(post => post.id === id);
 
@@ -45,8 +43,6 @@ export class JsonPostRepository implements PostRepository {
         return post;
     }
     async findBySlug(slug: string): Promise<PostModel> {
-        await this.simulateWait();
-
         const posts = await this.findAllPublic();
         const post = posts.find(post => post.slug === slug);
 
