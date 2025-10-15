8e26cf82c88ba252eda9198d3837de82c29166b2
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 15:48:00 -0300

    ✨ feat: títulos nas páginas de posts e not-found

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,19 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PostSlugPageProps = {
     params: Promise<{ slug: string }>
 }
+export async function generateMetadata({ params }: PostSlugPageProps):Promise<Metadata>{
+     const { slug } = await params
+    const post = await findPostBySlugCached(slug).catch(() => undefined)
 
+    return {
+        title: post?.title,
+        description: post?.excerpt
+    }
+}
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
     const { slug } = await params
     const post = await findPostBySlugCached(slug).catch(() => undefined)
