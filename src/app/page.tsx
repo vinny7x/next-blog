37a5f6726121821a2a9aee773ba32f7f66d8a767
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 15:55:18 -0300

    :recycle: refactor: removendo Suspense duplicado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ export default async function HomePage() {
         <>
             <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
                 <FeaturedPost />
-            </Suspense>
-            <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
                 <PostsList />
             </Suspense>
         </>
