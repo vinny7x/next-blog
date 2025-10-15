9fb29afd1d6e80cbc24fc3b0ae32425f95a71bbc
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 15:54:19 -0300

    :lipstick: style: melhorando CSS do spinLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import { Suspense } from "react";
 export default async function HomePage() {
     return (
         <>
-            <Suspense fallback={<SpinLoader />}>
+            <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
                 <FeaturedPost />
             </Suspense>
-            <Suspense fallback={<SpinLoader />}>
+            <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
                 <PostsList />
             </Suspense>
         </>
