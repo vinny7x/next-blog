9a36d3b2fd72dae1c8145036d3f0c2dd7840b5ad
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-15 18:50:13 -0300

    ✨ feat: melhorando as tables

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -17,7 +17,20 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
             'prose-img:mx-auto',
             'lg:prose-lg'
         )}>
-            <ReactMarkdown rehypePlugins={[rehypeSanitize]} remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+            <ReactMarkdown
+            rehypePlugins={[rehypeSanitize]}
+            remarkPlugins={[remarkGfm]}
+            components={{
+                table:({node, ...props}) => {
+                    if(!node?.children) return ''
+                    return (
+                        <div className="overflow-x-auto">
+                            <table className="w-full min-w-[600px]" {...props}/>
+                        </div>
+                    )
+                }
+            }}
+            >{markdown}</ReactMarkdown>
         </div>
     )
 }
