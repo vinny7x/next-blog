5084dfd331beb258afa989ebb94deff947fa12b9
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-16 16:35:08 -0300

    :wrench: chore: transformando componente em 'use client'

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,12 +6,14 @@ type RootErrorPageProps = {
     error: Error,
     reset: () => void
 }
-export default function RootErrorPage({error}:RootErrorPageProps){
-    useEffect(()=>{
+
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+    useEffect(() => {
         console.log(error)
-    },[error])
+    }, [error])
+
     return <ErrorMessage
-    pageTitle="Internal Server Error"
-    contenteTitle="501"
-    content="Ocorreu um erro do qual nossa aplicação não conseguiu se recuperrar. tente novamente mais tarde."/>
+        pageTitle="Internal Server Error"
+        contenteTitle="501"
+        content="Ocorreu um erro do qual nossa aplicação não conseguiu se recuperrar. tente novamente mais tarde." />
 }
