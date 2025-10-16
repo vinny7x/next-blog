676939dbf57a94cb6c78ba5ad0f817aa584d1016
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-16 10:56:30 -0300

    ✨ feat: componente ErrorMessage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,12 @@
+import ErrorMessage from "@/components/ErrorMessage";
 import clsx from "clsx";
 
 export default function NotFoundPage() {
     return (
-        <>
-            <title>Página não encontrada</title>
-            <div
-                className={clsx(
-                    "bg-slate-900 text-slate-100",
-                    "mb-16 p-8 rounded-xl",
-                    "flex flex-col items-center justify-center",
-                    "text-center gap-2",
-                    "min-h-[320px]"
-                )}
-            >
-                <h1 className="text-7xl font-extrabold leading-tight">404</h1>
-                <p className="text-lg text-slate-300">
-                    A página que você tentou acessar não foi encontrada.
-                </p>
-            </div>
-        </>
+        <ErrorMessage
+            pageTitle="Página não encontrada"
+            contenteTitle="404"
+            content="A página que você tentou acessar não foi encontrada."
+        />
     );
 }
---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+type ErrorMessageProps = {
+    pageTitle: string,
+    contenteTitle: string,
+    content: React.ReactNode
+}
+export default function ErrorMessage({ pageTitle, contenteTitle, content }: ErrorMessageProps) {
+    return (
+        <>
+            <title>{pageTitle}</title>
+            <div
+                className={clsx(
+                    "bg-slate-900 text-slate-100",
+                    "mb-16 p-8 rounded-xl",
+                    "flex flex-col items-center justify-center",
+                    "text-center gap-2",
+                    "min-h-[320px]"
+                )}
+            >
+                <h1 className="text-7xl font-extrabold leading-tight">{contenteTitle}</h1>
+                <div className="text-lg text-slate-300">
+                    {content}
+                </div>
+            </div>
+        </>
+    );
+}
