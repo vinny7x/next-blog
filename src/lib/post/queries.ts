1d997e9ce2e0c33409894268bed9bc615067617a
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-20 17:38:06 -0300

    ✨ feat: configurando o repositório com o drizzle

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -9,7 +9,7 @@ export const findAllPublicPostsCached = cache(
 )
 export const findPostBySlugCached = cache(
     async (slug: string) => {
-        const post = await postRepository.findBySlug(slug).catch(() => undefined)
+        const post = await postRepository.findBySlugPublic(slug).catch(() => undefined)
         if (!post) notFound()
         return post
     }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -46,7 +46,7 @@ export class JsonPostRepository implements PostRepository {
 
         return post;
     }
-    async findBySlug(slug: string): Promise<PostModel> {
+    async findBySlugPublic(slug: string): Promise<PostModel> {
         const posts = await this.findAllPublic();
         const post = posts.find(post => post.slug === slug);
 
