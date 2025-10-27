22ae15e6c852f06c43de9ab7dda6a35ac83f288e
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-10-26 22:40:37 -0300

    :wrench: chore: melhorando as queries públicas

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -3,19 +3,24 @@ import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 import { cache } from "react";
 
-export const findAllPublicPostsCached = unstable_cache(cache(
+export const findAllPublicPostsCached = cache(unstable_cache(
     async () => {
         return await postRepository.findAllPublic();
     }
-), ['posts'], {
+    , ['posts'], {
     tags: ['posts']
-});
-export const findPublicPostBySlugCached = (slug: string) => unstable_cache(cache(
-    async (slug: string) => {
-        const post = await postRepository.findBySlugPublic(slug).catch(() => undefined);
-        if (!post) notFound();
-        return post;
-    }
-), ['posts'], {
-    tags: [`post-${slug}`]
-})(slug);
+}));
+export const findPublicPostBySlugCached = cache((slug: string) => {
+    return unstable_cache(
+        async (slug: string) => {
+            const post = await postRepository
+                .findBySlugPublic(slug)
+                .catch(() => undefined);
+
+            if (!post) notFound();
+            return post
+        },
+        [`post-${slug}`],
+        { tags: [`post-${slug}`] }
+    )(slug);
+})
