6ca3eaaef95e295104c21cacb71c51acca9fa979
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-01 18:48:59 -0300

    ✨ feat: páginas de admin

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,7 +1,16 @@
+import PostsListAdmin from "@/components/PostsListAdmin";
+import { SpinLoader } from "@/components/SpinLoader";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
 export const dynamic = 'force-dynamic';
 
+export const metadata: Metadata = {
+    title: 'Post Admin'
+};
+
 export default async function AdminPostPage() {
-    return <div className="py-16 text-3xl">
-        AdminPostPage
-    </div>;
+    return <Suspense fallback={<SpinLoader />}>
+        <PostsListAdmin />
+    </Suspense>;
 }
