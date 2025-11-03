c8ff523c356c7d40a4083c9bdecae212f8d28415
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-02 21:15:03 -0300

    ✨ feat: configurando a action para deletar o post

--- src/components/Admin/DeletePostButton/index.tsx
+++ src/components/Admin/DeletePostButton/index.tsx
@@ -22,8 +22,11 @@ export function DeletePostButton({ id, title }: DeletePostButtonProps) {
     function handleConfirm() {
         startTransition(async () => {
             const result = await deletePostAction(id);
-            alert(result);
             setShowDialog(false);
+
+            if (result.error) {
+                alert(`Erro: ${result.error}`);
+            }
         });
     }
 
