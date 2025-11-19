0c4de253d3702fc33307bb535c26276d06d3f86f
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-18 22:12:35 -0300

    ✨ feat: adiciona suporte ao envio de imagens

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
-import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/constants";
+import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const [isUploading, startTransition] = useTransition();
     function handleChooseFile() {
+        toast.dismiss();
         if (!fileInputRef.current) return;
         fileInputRef.current.click();
     }
@@ -17,16 +20,21 @@ export function ImageUploader() {
         if (!fileInput) return;
         const file = fileInput.files?.[0];
         if (!file) return;
-        if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
-            toast.error(`Imagem muito grande, Máx.: ${IMAGE_UPLOADER_MAX_SIZE / 1024}KB`);
+        if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+            toast.error(`Imagem muito grande, Máx.: ${IMAGE_UPLOAD_MAX_SIZE / 1024}KB`);
             fileInput.value = '';
             return;
         }
         const formData = new FormData();
         formData.append('file', file);
-        // TODO: criar action para o envio de arquivos
-        console.log(formData.get('file'));
-
+        startTransition(async () => {
+            const result = await uploadImageAction(formData);
+            if (result.error) {
+                toast.error(result.error);
+                return;
+            }
+            toast.success(result.url);
+        });
         fileInput.value = '';
 
     }
