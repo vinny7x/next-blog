71863d1d51f5c3bdb155acd33bc23e34530b28e5
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-11-19 16:13:21 -0300

    ✨ feat: adiciona preview de imagem enviada

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -4,12 +4,14 @@ import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [isUploading, startTransition] = useTransition();
+    const [imgUrl, setImgUrl] = useState<string>('');
+
     function handleChooseFile() {
         toast.dismiss();
         if (!fileInputRef.current) return;
@@ -17,12 +19,19 @@ export function ImageUploader() {
     }
     function handleChange() {
         const fileInput = fileInputRef.current;
-        if (!fileInput) return;
+        if (!fileInput) {
+            setImgUrl('');
+            return;
+        }
         const file = fileInput.files?.[0];
-        if (!file) return;
+        if (!file) {
+            setImgUrl('');
+            return;
+        };
         if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
             toast.error(`Imagem muito grande, Máx.: ${IMAGE_UPLOAD_MAX_SIZE / 1024}KB`);
             fileInput.value = '';
+            setImgUrl('');
             return;
         }
         const formData = new FormData();
@@ -31,21 +40,31 @@ export function ImageUploader() {
             const result = await uploadImageAction(formData);
             if (result.error) {
                 toast.error(result.error);
+                setImgUrl('');
                 return;
             }
-            toast.success(result.url);
+            toast.success('Imagem enviada!');
+            setImgUrl(result.url);
         });
         fileInput.value = '';
 
     }
-    return <div className='flex flex-col gap-2 py-4'>
+    return <div className='flex flex-col gap-4 py-4'>
         <Button
             type="button"
             className="self-start"
             onClick={handleChooseFile}
+            disabled={isUploading}
         >
             <ImageUpIcon />
             Enviar uma imagem</Button>
-        <input onChange={handleChange} className='hidden' name='file' type='file' accept='image/*' ref={fileInputRef} />
+        {!!imgUrl && (
+            <div className="flex flex-col gap-4">
+                <p>URL: {imgUrl}</p>
+                {/* eslint-disable-next-line */}
+                <img className="rounded-lg w-20" src={imgUrl} alt="Imagem enviada" />
+            </div>
+        )}
+        <input onChange={handleChange} className='hidden' name='file' type='file' accept='image/*' ref={fileInputRef} disabled={isUploading}/>
     </div>;
 }
