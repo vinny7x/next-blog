e9af076b7b663bc35f29ab3e50f542c283c3fef7
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-03 23:06:50 -0300

    ✨ feat: adiciona suporte a imagens locais

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/**',
+                search: ''
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/**',
+                search: ''
+            }
+        ]
+    }
 };
 
 export default nextConfig;
