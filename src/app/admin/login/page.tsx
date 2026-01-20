88cab25ef198d724ef866268b4eb38e7262d9925
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-20 19:08:38 -0300

    ✨ feat: cria a página para login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,7 @@
+import { LoginForm } from "@/components/Admin/LoginForm";
+
 export const dynamic = 'force-dynamic';
 
 export default async function AdminLoginPage() {
-    return <div className="py-16 text-3xl">
-        AdminLoginPage
-    </div>;
+    return <LoginForm />
 }
