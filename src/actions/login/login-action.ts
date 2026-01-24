c4ba5e9199a1ce1310aca73999dfc32a7246e01c
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-24 00:07:24 -0300

    ✨ feat: cria a action para fazer login

---
+++ src/actions/login/login-action.ts
@@ -0,0 +1,42 @@
+'use server';
+
+import { createLoginSession, verifyPassword } from "@/lib/login/manage-login";
+import { asyncDelay } from "@/utils/async-delay";
+import { redirect } from "next/navigation";
+
+type LoginActionState = {
+    username: string,
+    error: string;
+};
+
+export async function loginAction(state: LoginActionState, formData: FormData) {
+    await asyncDelay(5000);
+
+    if (!(formData instanceof FormData)) {
+        return {
+            username: '',
+            error: 'Dados inválidos'
+        };
+    }
+
+    const username = formData.get('username')?.toString().trim() || '';
+    const password = formData.get('password')?.toString().trim() || '';
+
+    if (!username || !password) {
+        return {
+            username,
+            error: 'Digite o usuário e a senha'
+        };
+    }
+
+    const isUsernameValid = username === process.env.LOGIN_USER;
+    const isPasswordValid = await verifyPassword(password, process.env.LOGIN_PASS || '');
+    if (!isUsernameValid || !isPasswordValid) {
+        return {
+            username,
+            error: 'Usuário ou senha inválidos'
+        };
+    }
+    await createLoginSession(username);
+    redirect('/admin/post')
+}
