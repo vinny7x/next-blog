36cd6d33879aca2c6674df503e7b8d0d2a449deb
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-23 23:33:51 -0300

    ✨ feat: cria função para criação e exclusão do cookie de login

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,4 +1,8 @@
 import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+
+const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
+const loginCookieName = process.env.LOGIN_COOKIE_NAME || 'loginSession';
 
 export async function hashPassword(password: string) {
     const hash = await bcrypt.hash(password, 10);
@@ -10,3 +14,20 @@ export async function verifyPassword(password: string, base64Hash: string) {
     return await bcrypt.compare(password, hash);
 }
 
+export async function createLoginSession(username: string) {
+    const expiresAt = new Date(Date.now() + loginExpSeconds * 1000);
+    const loginSession = username + 'QUALQUER COISA';
+
+    const cookieStore = await cookies();
+    cookieStore.set(loginCookieName, loginSession, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        expires: expiresAt
+    });
+}
+export async function deleteLoginSession() {
+    const cookieStore = await cookies();
+    cookieStore.set(loginCookieName, '', { expires: new Date(0) });
+    cookieStore.delete(loginCookieName);
+}
