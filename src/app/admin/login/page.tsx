f04570e8e8b75dfec644e421b3e5296ed18dda59
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-30 21:43:54 -0300

    ✨ feat: adiciona o bloqueio de login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,19 @@
 import { LoginForm } from "@/components/Admin/LoginForm";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminLoginPage() {
-    return <LoginForm />
+    const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN));
+
+    if (!allowLogin) {
+        return (
+            <ErrorMessage
+                contenteTitle="403"
+                content='Libere o sistema de login usando ALLOW_LOGIN' />
+        );
+
+    }
+
+    return <LoginForm />;
 }
