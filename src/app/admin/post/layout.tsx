5aecd0a584b4aab63fee94b4094c585ddc098dff
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-01-31 21:16:56 -0300

    ✨ feat: adiciona verificação no layout das páginas administrativas

--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,11 +1,13 @@
 import { MenuAdmin } from "@/components/Admin/MenuAdmin";
+import { requireLoginSessionOrRedirecty } from "@/lib/login/manage-login";
 
 type AdminPostsLayuotProps = {
     children: React.ReactNode;
 };
 
 
-export default function AdminPostsLayuot({ children }: Readonly<AdminPostsLayuotProps>) {
+export default async function AdminPostsLayuot({ children }: Readonly<AdminPostsLayuotProps>) {
+    await requireLoginSessionOrRedirecty()
     return <>
         <MenuAdmin />
         {children}
