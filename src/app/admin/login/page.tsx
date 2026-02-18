63fcf3f02b24d632f234205fd92044eefaee2ce8
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-02-18 18:02:32 -0300

    ✨ feat: adiciona titulo na página de login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,12 @@
 import { LoginForm } from "@/components/Admin/LoginForm";
 import ErrorMessage from "@/components/ErrorMessage";
+import { Metadata } from "next";
 
 export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+    title: 'Login'
+};
 
 export default async function AdminLoginPage() {
     const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN));
@@ -12,7 +17,6 @@ export default async function AdminLoginPage() {
                 contenteTitle="403"
                 content='Libere o sistema de login usando ALLOW_LOGIN' />
         );
-
     }
 
     return <LoginForm />;
